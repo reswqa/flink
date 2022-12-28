4a824eb76de2538651dcbaa2b564c047d685e7e5
Wencong Liu <liuwenclever@163.com>
2022-12-28 14:57:59 +0800

    [TS] remove unnecessary lock

--- flink-runtime/src/main/java/org/apache/flink/runtime/io/network/partition/store/reader/TieredStoreConsumerImpl.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/io/network/partition/store/reader/TieredStoreConsumerImpl.java
@@ -18,7 +18,6 @@
 
 package org.apache.flink.runtime.io.network.partition.store.reader;
 
-import org.apache.flink.runtime.io.network.buffer.Buffer;
 import org.apache.flink.runtime.io.network.partition.BufferAvailabilityListener;
 import org.apache.flink.runtime.io.network.partition.ResultSubpartition.BufferAndBacklog;
 import org.apache.flink.runtime.io.network.partition.ResultSubpartitionView;
@@ -30,13 +29,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
-import javax.annotation.concurrent.GuardedBy;
 
 import java.io.IOException;
-import java.util.Arrays;
 
 import static org.apache.flink.util.Preconditions.checkArgument;
-import static org.apache.flink.util.Preconditions.checkNotNull;
 
 /** The reader of Tiered Store. */
 public class TieredStoreConsumerImpl implements TieredStoreConsumer {
@@ -47,17 +43,10 @@ public class TieredStoreConsumerImpl implements TieredStoreConsumer {
 
     private final BufferAvailabilityListener availabilityListener;
 
-    private final Object lock = new Object();
-
     private final SingleTierDataGate[] tierDataGates;
 
     private final SingleTierReader[] singleTierReaders;
 
-    @Nullable
-    @GuardedBy("lock")
-    private Throwable failureCause = null;
-
-    @GuardedBy("lock")
     private boolean isReleased = false;
 
     private int currentSegmentIndex = 0;
@@ -97,76 +86,71 @@ public class TieredStoreConsumerImpl implements TieredStoreConsumer {
     @Nullable
     @Override
     public BufferAndBacklog getNextBuffer() throws IOException {
-        try {
-            synchronized (lock) {
-                if (!findTierContainsNextSegment()) {
-                    LOG.warn(
-                            String.format(
-                                    "Can not find a tier contains the next segment %d for subpartition %d.",
-                                    currentSegmentIndex, subpartitionId));
-                    return null;
-                }
-
-                BufferAndBacklog bufferAndBacklog =
-                        singleTierReaders[viewIndexContainsCurrentSegment].getNextBuffer();
-
-                if (bufferAndBacklog != null) {
-                    //bufferAndBacklog.setNextDataType(Buffer.DataType.DATA_BUFFER);
-                    hasSegmentFinished = bufferAndBacklog.isLastBufferInSegment();
-                    if (hasSegmentFinished) {
-                        currentSegmentIndex++;
-                    }
-                    bufferAndBacklog.setSequenceNumber(currentSequenceNumber);
-                    currentSequenceNumber++;
-                }
-                return bufferAndBacklog;
-            }
-        } catch (Throwable cause) {
-            // release subpartition reader outside of lock to avoid deadlock.
-            releaseInternal(cause);
-            throw new IOException("Faild to get next buffer in reader.", cause);
+        if (!findTierContainsNextSegment()) {
+            return null;
         }
+
+        BufferAndBacklog bufferAndBacklog =
+                singleTierReaders[viewIndexContainsCurrentSegment].getNextBuffer();
+
+        if (bufferAndBacklog != null) {
+            //bufferAndBacklog.setNextDataType(Buffer.DataType.DATA_BUFFER);
+            hasSegmentFinished = bufferAndBacklog.isLastBufferInSegment();
+            if (hasSegmentFinished) {
+                currentSegmentIndex++;
+            }
+            bufferAndBacklog.setSequenceNumber(currentSequenceNumber);
+            currentSequenceNumber++;
+        }
+        return bufferAndBacklog;
     }
 
     @Override
     public ResultSubpartitionView.AvailabilityWithBacklog getAvailabilityAndBacklog(
             int numCreditsAvailable) {
-        synchronized (lock) {
-            // first scan all result subpartition views
-            for (SingleTierReader singleTierReader : singleTierReaders) {
-                singleTierReader.getAvailabilityAndBacklog(numCreditsAvailable);
-            }
-            if (findTierContainsNextSegment()) {
-                LOG.debug(
-                        "%%% find the subpartitionId {}, currentSegmentIndex {}, successful!",
-                        subpartitionId, currentSegmentIndex);
-                return singleTierReaders[viewIndexContainsCurrentSegment].getAvailabilityAndBacklog(
-                        numCreditsAvailable);
-            }
-            LOG.debug(
-                    "%%% find the subpartitionId {}, currentSegmentIndex {}, not finded",
-                    subpartitionId, currentSegmentIndex);
-            return new ResultSubpartitionView.AvailabilityWithBacklog(false, 0);
+        // first scan all result subpartition views
+        for (SingleTierReader singleTierReader : singleTierReaders) {
+            singleTierReader.getAvailabilityAndBacklog(numCreditsAvailable);
         }
+        if (findTierContainsNextSegment()) {
+            return singleTierReaders[viewIndexContainsCurrentSegment].getAvailabilityAndBacklog(
+                    numCreditsAvailable);
+        }
+        return new ResultSubpartitionView.AvailabilityWithBacklog(false, 0);
     }
 
     @Override
     public void releaseAllResources() throws IOException {
-        releaseInternal(null);
+        if (isReleased) {
+            return;
+        }
+        isReleased = true;
+        for (int i = 0; i < singleTierReaders.length; i++) {
+            if (singleTierReaders[i] != null) {
+                try {
+                    singleTierReaders[i].releaseAllResources();
+                } catch (IOException ioException) {
+                    throw new RuntimeException(
+                            "Failed to release partition view resources.", ioException);
+                }
+            }
+        }
     }
 
     @Override
     public boolean isReleased() {
-        synchronized (lock) {
-            return isReleased;
-        }
+        return isReleased;
     }
 
     @Override
     public Throwable getFailureCause() {
-        synchronized (lock) {
-            return failureCause;
+        for(SingleTierReader singleTierReader : singleTierReaders){
+            Throwable failureCause = singleTierReader.getFailureCause();
+            if( failureCause != null){
+                return failureCause;
+            }
         }
+        return null;
     }
 
     @Override
@@ -178,10 +162,8 @@ public class TieredStoreConsumerImpl implements TieredStoreConsumer {
 
     @Override
     public int getNumberOfQueuedBuffers() {
-        synchronized (lock) {
-            findTierContainsNextSegment();
-            return singleTierReaders[viewIndexContainsCurrentSegment].getNumberOfQueuedBuffers();
-        }
+        findTierContainsNextSegment();
+        return singleTierReaders[viewIndexContainsCurrentSegment].getNumberOfQueuedBuffers();
     }
 
     // -------------------------------
@@ -194,53 +176,18 @@ public class TieredStoreConsumerImpl implements TieredStoreConsumer {
             singleTierReader.getAvailabilityAndBacklog(Integer.MAX_VALUE);
         }
 
-        LOG.debug(
-                "%%% {} Trying to find the segment index {} in which tier",
-                taskName, currentSegmentIndex);
-
         if (!hasSegmentFinished) {
-            LOG.debug("%%% {} segment not finished", taskName);
             return true;
         }
 
         for (int i = 0; i < tierDataGates.length; i++) {
             SingleTierDataGate tieredDataGate = tierDataGates[i];
             if (tieredDataGate.hasCurrentSegment(subpartitionId, currentSegmentIndex)) {
-                LOG.debug(
-                        "%%% {} Successfully find the segment index {} in tier {}",
-                        taskName, currentSegmentIndex, i);
                 viewIndexContainsCurrentSegment = i;
                 return true;
             }
         }
 
         return false;
-    }
-
-    private void releaseInternal(@Nullable Throwable throwable) {
-        boolean[] toReleaseViews = new boolean[singleTierReaders.length];
-        Arrays.fill(toReleaseViews, false);
-        synchronized (lock) {
-            if (isReleased) {
-                return;
-            }
-            isReleased = true;
-            failureCause = throwable;
-            for (int i = 0; i < singleTierReaders.length; i++) {
-                if (singleTierReaders[i] != null) {
-                    toReleaseViews[i] = true;
-                }
-            }
-        }
-        for (int i = 0; i < toReleaseViews.length; i++) {
-            if (toReleaseViews[i]) {
-                try {
-                    checkNotNull(singleTierReaders[i]).releaseAllResources();
-                } catch (IOException ioException) {
-                    throw new RuntimeException(
-                            "Failed to release partition view resources.", ioException);
-                }
-            }
-        }
     }
 }
